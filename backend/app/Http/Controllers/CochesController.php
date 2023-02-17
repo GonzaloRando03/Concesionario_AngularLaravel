91fdef34d1341046f7604912c603d99bb4b60af5
Gonzalo Rando <gonzalorando03@gmail.com>
2023-02-17 08:39:35 +0100

    firstCommit

---
+++ backend/app/Http/Controllers/CochesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Coches;
+use Illuminate\Http\Request;
+
+class CochesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Coches  $coches
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Coches $coches)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Coches  $coches
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Coches $coches)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Coches  $coches
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Coches $coches)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Coches  $coches
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Coches $coches)
+    {
+        //
+    }
+}
---
+++ backend/database/factories/CochesFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Coches;
+use Faker\Generator as Faker;
+
+$factory->define(Coches::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
